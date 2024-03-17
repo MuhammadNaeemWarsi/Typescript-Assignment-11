f19794fca45668a873b9322e6ef3e280914a9f81
[muhammad naeem] <[naeemwarsi20@gmail/com]>
2024-03-17 07:07:18 +0500

    Assignment 11

---
+++ Q11.ts
@@ -0,0 +1,8 @@
+// Names: Store the names of a few of your friends in a array called names.
+// Print each person’s name by accessing each element in the list, one at a time.
+
+let friendNames: string []= ["Hazrat Abu Bakar","Hazrat Umar","Hazrat Usman","Hazrat Ali"];
+console.log(friendNames[0])
+console.log(friendNames[1])
+console.log(friendNames[2])
+console.log(friendNames[3])
